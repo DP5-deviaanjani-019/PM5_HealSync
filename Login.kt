83582849ba54fb35211d2aa571f1d10c8184a158
DP5-deviaanjani-019 <114506379+DP5-deviaanjani-019@users.noreply.github.com>
2024-10-25 20:38:55 +0700

    Login.kt 1

---
+++ Login.kt
@@ -0,0 +1,11 @@
+package com.example.mylogin
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class Login : AppCompatActivity() {
+    private val correctUsername = "admin"
+    private val correctPassword = "1234"
